621de9b9da76cfc54ba28cc6d242b0c762513b85
Sushil Kumar <83951653+SushilKumar045@users.noreply.github.com>
2024-08-01 16:05:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,69 +1,68 @@
-import streamlit as st 
+import streamlit as st
 from PIL import Image
-import pickle
+import joblib
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.tree import DecisionTreeClassifier
 
-import joblib
+# Load dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, 1:4].values
+y = dataset.iloc[:, -1].values
+
+# Encode categorical data
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+
+# Standardize features
+sc = StandardScaler()
+X = sc.fit_transform(X)
+
+# Instantiate and train the model
+model = DecisionTreeClassifier()
+model.fit(X, y)
+
 # Save the model
 joblib.dump(model, 'Project3_Decision_Tree_model.pkl')
+
 # Load the model
 model = joblib.load('Project3_Decision_Tree_model.pkl')
 
-# st.set_option('deprecation.showfileUploaderEncoding', False)
-# Load the pickled model
-# pickle_in = open("Project3_Decision_Tree_model.pkl","rb")
-# model=pickle.load(pickle_in)
-
-dataset= pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:, 1:4].values
-from sklearn.preprocessing import LabelEncoder
-labelencoder_X = LabelEncoder()
-X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X = sc.fit_transform(X)
-def predict_note_authentication(UserID, Gender,Age,EstimatedSalary):
-  output= model.predict(sc.transform([[Gender,Age,EstimatedSalary]]))
-  print("Purchased", output)
-  if output==[1]:
-    prediction="Item will be purchased"
-  else:
-    prediction="Item will not be purchased"
-  print(prediction)
-  return prediction
+def predict_note_authentication(UserID, Gender, Age, EstimatedSalary):
+    input_data = sc.transform([[Gender, Age, EstimatedSalary]])
+    output = model.predict(input_data)
+    print("Purchased", output)
+    if output == [1]:
+        prediction = "Item will be purchased"
+    else:
+        prediction = "Item will not be purchased"
+    print(prediction)
+    return prediction
 
 def main():
-    
     html_temp = """
-   <div class="" style="background-color:blue;" >
-   <div class="clearfix">           
-   <div class="col-md-12">
-   <center><p style="font-size:40px;color:white;margin-top:10px;">Poornima Institute of Engineering & Technology</p></center> 
-   <center><p style="font-size:30px;color:white;margin-top:10px;">Department of Computer Engineering</p></center> 
-   <center><p style="font-size:25px;color:white;margin-top:10px;">Internship Project Deployment</p></center> 
-   </div>
-   </div>
-   </div>
-   """
-    st.markdown(html_temp,unsafe_allow_html=True)
+    <div style="background-color:blue;padding:10px">
+    <h2 style="color:white;text-align:center;">Poornima Institute of Engineering & Technology</h2>
+    <h3 style="color:white;text-align:center;">Department of Computer Engineering</h3>
+    <h4 style="color:white;text-align:center;">Internship Project Deployment</h4>
+    </div>
+    """
+    st.markdown(html_temp, unsafe_allow_html=True)
     st.header("Item Purchase Prediction")
-    UserID = st.text_input("UserID","")
-    Gender = st.selectbox('Gender',('Male', 'Female'))
-    if Gender == 'Male':
-      Gender = 1
-    else:
-      Gender = 0
-    Age = st.number_input("Insert Age",18,60)
-    EstimatedSalary = st.number_input("Insert salary",15000,150000)
-    result=""
+    UserID = st.text_input("UserID", "")
+    Gender = st.selectbox('Gender', ('Male', 'Female'))
+    Gender = 1 if Gender == 'Male' else 0
+    Age = st.number_input("Insert Age", 18, 60)
+    EstimatedSalary = st.number_input("Insert salary", 15000, 150000)
+    result = ""
     if st.button("Predict"):
-      result=predict_note_authentication(UserID, Gender,Age,EstimatedSalary)
-      st.success('Model has predicted {}'.format(result))
+        result = predict_note_authentication(UserID, Gender, Age, EstimatedSalary)
+        st.success('Model has predicted: {}'.format(result))
     if st.button("About"):
-      st.subheader("Developed by Sushil Kumar")
-      st.subheader("Student , Department of Artificial Intelligence and Data Science")
+        st.subheader("Developed by Sushil Kumar")
+        st.subheader("Student, Department of Artificial Intelligence and Data Science")
 
-if __name__=='__main__':
-  main()
+if __name__ == '__main__':
+    main()
